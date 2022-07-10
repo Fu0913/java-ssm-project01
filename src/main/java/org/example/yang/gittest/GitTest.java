d3fe8a00bdd0539a8d8f9332820724b0ae972a51
yun <yun@athome.com>
2022-07-10 20:34:12 +0800

    Changes

---
+++ src/main/java/org/example/yang/gittest/GitTest.java
@@ -0,0 +1,8 @@
+package org.example.yang.gittest;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello worl");
+        System.out.println("hello worl");
+    }
+}
